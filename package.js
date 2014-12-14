d728decf6a0833c4b37db51858bdd88d4fb19dc0
Brian Dunnette <brian@dunnette.us>
2014-12-14 17:14:47 -0600

    initial commit

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'bdunnette:flakes',
+  summary: ' /* Fill me in! */ ',
+  version: '1.0.0',
+  git: ' /* Fill me in! */ '
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.1');
+  api.addFiles('js/base.js', 'client');
+  api.addFiles('js/utils.js', 'client');
+  api.addFiles('css/all.css', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('meteor-flakes');
+  api.addFiles('meteor-flakes-tests.js');
+});
